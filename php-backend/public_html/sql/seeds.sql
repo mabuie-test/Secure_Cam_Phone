bf2eba8c850954a2a887bd6cf166bad5beb7475d
Jorge Augusto Mabuie <jorgemaabuie@gmail.com>
2026-01-16 11:47:06 +0200

    Add user seed data to seeds.sql
    Insert initial user data into the users table.

---
+++ php-backend/public_html/sql/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, password_hash) VALUES
+('jorgemaabuie@gmail.com', '{'846696',PASSWORD_DEFAULT}');
+-- Substitui {PASSWORD_HASH_PLACEHOLDER} por password_hash('s3nhA', PASSWORD_DEFAULT) ou gera no PHP e cola aqui.
